173a7e89d0cfeb3676204a249de7fa9201ca7d47
gopinath <jayachandran.gopinath@divum.in>
2024-07-25 15:05:09 +0530

    check*&^/

--- src/main/java/com/example/jenkinsspring/controller.java
+++ src/main/java/com/example/jenkinsspring/controller.java
@@ -3,7 +3,7 @@ package com.example.jenkinsspring;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+@RestController
 public class controller {
 
     @GetMapping("")
